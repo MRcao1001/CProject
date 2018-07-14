28c22f5616779133ed203c0e00fa57413be471ce
MRcao1001 <cy15056266084@outlook.com>
2018-07-14 15:56:31 +0800

    714commit

--- CProject/CExercise/100c_exercise/6.c
+++ CProject/CExercise/100c_exercise/6.c
@@ -1,6 +1,7 @@
 /*题目：用*号输出字母C的图案。 
       1.程序分析：可先用'*'号在纸上写出字母C，再分行输出。 
-      2.程序源代码： */
+      2.程序源代码:
+      */
 #include <stdio.h>
 #include <stdlib.h>
 void printwords(int size, char words[64])
